baa6fe4c9c6a6f432aaf68d0b61f86fb1837efe1
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2023-12-26 18:37:35 +0330

    Removed global variables and passed necessary parameters as arguments to the function.

--- Unsupervised Learning/Bisecting K-Means VS Regular K-Means/main.py
+++ Unsupervised Learning/Bisecting K-Means VS Regular K-Means/main.py
@@ -15,26 +15,10 @@ def plot_clusters(ax, X, algo, centers, n_clusters, algorithm_name):
     ax.scatter(centers[:, 0], centers[:, 1], c="r", s=20)
     ax.set_title(f"{algorithm_name} : {n_clusters} clusters")
 
-def main():
-    # Parameters
-    n_samples = 10000
-    random_state = 0
-    n_clusters_list = [4, 8, 16]
-
-    # Generate sample data
-    X, _ = generate_sample_data(n_samples, random_state)
-
-    # Algorithms to compare
-    clustering_algorithms = {
-        "Bisecting K-Means": BisectingKMeans,
-        "K-Means": KMeans,
-    }
-
+def compare_clustering_algorithms(X, random_state, n_clusters_list, clustering_algorithms):
+    """Compare different clustering algorithms and plot the results."""
     # Make subplots for each variant
-    fig, axs = plt.subplots(
-        len(clustering_algorithms), len(n_clusters_list), figsize=(12, 5)
-    )
-
+    fig, axs = plt.subplots(len(clustering_algorithms), len(n_clusters_list), figsize=(12, 5))
     axs = axs.T
 
     # Compare clustering algorithms
@@ -56,6 +40,23 @@ def main():
 
     # Show the plot
     plt.show()
+
+def main():
+    # Parameters
+    n_samples = 10000
+    random_state = 0
+    n_clusters_list = [4, 8, 16]
+
+    # Generate sample data
+    X, _ = generate_sample_data(n_samples, random_state)
+
+    # Algorithms to compare
+    clustering_algorithms = {
+        "Bisecting K-Means": BisectingKMeans,
+        "K-Means": KMeans,
+    }
+
+    compare_clustering_algorithms(X, random_state, n_clusters_list, clustering_algorithms)
 
 if __name__ == "__main__":
     main()
