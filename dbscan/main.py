a9d0becfd2ea5ecf26cf922b7ddce32df106bc85
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2023-12-27 17:29:22 +0330

    Enhance clarity and structure in DBSCAN clustering demonstration.

--- dbscan/main.py
+++ dbscan/main.py
@@ -10,19 +10,19 @@ class DBSCANDemo:
         self.X, self.labels_true = self._generate_synthetic_data()
 
     def _generate_synthetic_data(self):
-        # Data generation
+        # Generate synthetic data
         centers = [[1, 1], [-1, -1], [1, -1]]
         X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4, random_state=0)
         X = StandardScaler().fit_transform(X)
         return X, labels_true
 
     def visualize_data(self):
-        # Visualization of the resulting data
+        # Visualize the data
         plt.scatter(self.X[:, 0], self.X[:, 1])
         plt.show()
 
     def apply_dbscan(self, eps=0.3, min_samples=10):
-        # Compute DBSCAN
+        # Apply DBSCAN clustering
         db = DBSCAN(eps=eps, min_samples=min_samples).fit(self.X)
         labels = db.labels_
         n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
@@ -34,7 +34,7 @@ class DBSCANDemo:
         return labels, n_clusters_, db
 
     def evaluate_clusters(self, labels):
-        # Evaluation metrics
+        # Evaluate clustering metrics
         print(f"Homogeneity: {metrics.homogeneity_score(self.labels_true, labels):.3f}")
         print(f"Completeness: {metrics.completeness_score(self.labels_true, labels):.3f}")
         print(f"V-measure: {metrics.v_measure_score(self.labels_true, labels):.3f}")
@@ -43,7 +43,7 @@ class DBSCANDemo:
         print(f"Silhouette Coefficient: {metrics.silhouette_score(self.X, labels):.3f}")
 
     def plot_results(self, labels, db):
-        # Plot results
+        # Plot clustering results
         unique_labels = set(labels)
         core_samples_mask = np.zeros_like(labels, dtype=bool)
         core_samples_mask[db.core_sample_indices_] = True
@@ -57,7 +57,6 @@ class DBSCANDemo:
 
             xy = self.X[class_member_mask & core_samples_mask]
             plt.plot(xy[:, 0], xy[:, 1], "o", markerfacecolor=tuple(col), markeredgecolor="k", markersize=14)
-            
 
             xy = self.X[class_member_mask & ~core_samples_mask]
             plt.plot(xy[:, 0], xy[:, 1], "o", markerfacecolor=tuple(col), markeredgecolor="k", markersize=6)
