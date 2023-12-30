705858a6fb51bb8ac1db56b6dab4bb3125f55a23
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2023-12-30 17:57:12 +0330

    Refactor code for better readability and maintainability. Introduce named variables for max_depth values in the main function. No functional changes.

--- Decision Tree/main.py
+++ Decision Tree/main.py
@@ -30,13 +30,16 @@ def plot_results(X, y, X_test):
 def main():
     X, y = generate_random_dataset()
 
-    regr_1 = train_decision_tree(X, y, max_depth=2)
-    regr_2 = train_decision_tree(X, y, max_depth=5)
+    max_depth_1 = 2
+    max_depth_2 = 5
+
+    regr_1 = train_decision_tree(X, y, max_depth_1)
+    regr_2 = train_decision_tree(X, y, max_depth_2)
 
     X_test = np.arange(0.0, 5.0, 0.01)[:, np.newaxis]
 
-    predict_and_plot(regr_1, X_test, label=2, color="cornflowerblue")
-    predict_and_plot(regr_2, X_test, label=5, color="yellowgreen")
+    predict_and_plot(regr_1, X_test, label=max_depth_1, color="cornflowerblue")
+    predict_and_plot(regr_2, X_test, label=max_depth_2, color="yellowgreen")
 
     plot_results(X, y, X_test)
 
