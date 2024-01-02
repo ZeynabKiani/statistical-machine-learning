a1dd2934195ad66566007d9f390c3c7b3d739885
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2024-01-02 22:01:24 +0330

    Enhance comments for better code documentation

--- Decision Tree/main.py
+++ Decision Tree/main.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 
 def generate_random_dataset():
+
     
     rng = np.random.RandomState(1)
     X = np.sort(5 * rng.rand(80, 1), axis=0)
