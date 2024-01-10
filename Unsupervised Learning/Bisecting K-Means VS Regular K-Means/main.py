4feab8fdf7b692a2a48a5cc10ce72a94ed40dbde
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2024-01-10 23:49:13 +0330

    Plot clusters using Bisecting K-Means and K-Means algorithms

--- Unsupervised Learning/Bisecting K-Means VS Regular K-Means/main.py
+++ Unsupervised Learning/Bisecting K-Means VS Regular K-Means/main.py
@@ -3,7 +3,6 @@ from sklearn.cluster import BisectingKMeans, KMeans
 from sklearn.datasets import make_blobs
 
 def generate_sample_data(n_samples, random_state):
-    
     """Generate synthetic data with two centers."""
     return make_blobs(n_samples=n_samples, centers=2, random_state=random_state)
 
